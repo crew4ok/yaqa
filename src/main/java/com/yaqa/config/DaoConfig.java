f1c3043e3400869798b880017456e7486a20e801
Aleksandr Oksenenko <crew4ok@gmail.com>
2015-01-11 20:29:10 +0200

    Add different profiles for local and heroku envs

--- src/main/java/com/yaqa/config/DaoConfig.java
+++ src/main/java/com/yaqa/config/DaoConfig.java
@@ -2,10 +2,13 @@ package com.yaqa.config;
 
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
@@ -16,22 +19,72 @@ import javax.sql.DataSource;
 import java.net.MalformedURLException;
 
 @Configuration
-@ComponentScan({"com.yaqa.dao"})
+@ComponentScan("com.yaqa.dao")
 public class DaoConfig {
 
-    @Value("#{ systemEnvironment['DATABASE_URL'] }")
-    private String databaseUrl;
+    @Configuration
+    @Profile("heroku")
+    public static class HerokuDatabaseConfig {
+        @Value("#{ systemEnvironment['DATABASE_URL'] }")
+        private String databaseUrl;
+
+        @Bean(name = "dbUsername")
+        public String dbUsername() {
+            // TODO: rewrite (e.g. with regexp)
+            return databaseUrl.split("//")[1].split(":")[0];
+        }
+
+        @Bean(name = "dbPassword")
+        public String dbPassword() {
+            // TODO: rewrite (e.g. with regexp)
+            return databaseUrl.split("//")[1].split(":")[1].split("@")[0];
+        }
+
+        @Bean(name = "dbUrl")
+        public String dbUrl() {
+            return "jdbc:postgresql://" + databaseUrl.split("@")[1];
+        }
+    }
+
+    @Configuration
+    @Profile("local")
+    public static class LocalDatabaseConfig {
+        @Bean(name = "dbUsername")
+        public String dbUsername() {
+            System.out.println("activating local");
+            return "yaqa";
+        }
+
+        @Bean(name = "dbPassword")
+        public String dbPassword() {
+            return "yaqa";
+        }
+
+        @Bean(name = "dbUrl")
+        public String dbUrl() {
+            return "jdbc:postgresql://localhost:5432/yaqa";
+        }
+
+    }
+
+    @Autowired
+    @Qualifier("dbUsername")
+    private String dbUsername;
+
+    @Autowired
+    @Qualifier("dbPassword")
+    private String dbPassword;
+
+    @Autowired
+    @Qualifier("dbUrl")
+    private String dbUrl;
 
     @Bean
-    public DataSource dataSource() throws MalformedURLException {
-        String username = databaseUrl.split("//")[1].split(":")[0];
-        String password = databaseUrl.split("//")[1].split(":")[1].split("@")[0];
-        String dbUrl = "jdbc:postgresql://" + databaseUrl.split("@")[1];
-
+    public DataSource dataSource() {
         HikariConfig config = new HikariConfig();
         config.setJdbcUrl(dbUrl);
-        config.setUsername(username);
-        config.setPassword(password);
+        config.setUsername(dbUsername);
+        config.setPassword(dbPassword);
 
         return new HikariDataSource(config);
     }
