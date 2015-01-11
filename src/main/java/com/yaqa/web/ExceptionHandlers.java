72bb0ff5062c28ff6b276a10f9f33cb1f40c2356
Aleksandr Oksenenko <crew4ok@gmail.com>
2015-01-11 21:09:53 +0200

    Add logging in exception handlers

--- src/main/java/com/yaqa/web/ExceptionHandlers.java
+++ src/main/java/com/yaqa/web/ExceptionHandlers.java
@@ -1,25 +1,29 @@
 package com.yaqa.web;
 
-import com.yaqa.exception.NotFoundException;
 import com.yaqa.exception.YaqaBaseException;
 import com.yaqa.web.model.ErrorResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @ControllerAdvice(annotations = RestController.class)
 public class ExceptionHandlers {
+    private static final Logger LOG = LoggerFactory.getLogger(ExceptionHandlers.class);
+
 
     @ExceptionHandler(YaqaBaseException.class)
-    public ResponseEntity<ErrorResponse> notFoundException(YaqaBaseException e) {
+    public ResponseEntity<ErrorResponse> yaqaExceptionHandler(YaqaBaseException e) {
+        LOG.warn("yaqa exception", e);
         return new ResponseEntity<>(new ErrorResponse(e.getMessage()), HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(Throwable.class)
     public ResponseEntity<String> unexpectedError(Throwable t) {
+        LOG.error("unexpected error", t);
         return new ResponseEntity<>("unexpected error", HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
